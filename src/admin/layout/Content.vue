2085146ff06044194efa57281ee4156086f3fd91
ikaiguang <ikaiguang@163.com>
2019-01-09 07:32:10 +0000

    简单的admin-template

--- src/admin/layout/Content.vue
+++ src/admin/layout/Content.vue
@@ -3,9 +3,48 @@
         <b-container fluid>
             <b-row>
                 <b-col>
-                    <b-card>
-                        This is some text within the default card block.
-                    </b-card>
+                    <div class="widget-box">
+                        <div class="widget-title">
+                            <span class="icon"><i class="fa fa-arrows"></i></span>
+                            <h5>This a Title</h5>
+                        </div>
+                        <div class="widget-content">
+                            padding.
+                        </div>
+                    </div>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <div class="widget-box">
+                        <div class="widget-title">
+                            <span class="icon"><i class="fa fa-amazon"></i></span>
+                            <h5>This a Title</h5>
+                            <span class="label">
+                                <b-badge variant="success">4</b-badge>
+                                <b-badge pill variant="primary">Primary</b-badge>
+                            </span>
+                        </div>
+                        <div class="widget-content">
+                            padding.
+                        </div>
+                    </div>
+                </b-col>
+                <b-col>
+                    <div class="widget-box">
+                        <div class="widget-title">
+                            <span class="icon"><i class="fa fa-keyboard-o"></i></span>
+                            <h5>This a Title</h5>
+                            <span class="label">
+                                <b-link href="#"><b-badge variant="primary">Link1</b-badge></b-link>
+                                |
+                                <b-link href="#"><b-badge variant="success">Link2</b-badge></b-link>
+                            </span>
+                        </div>
+                        <div class="widget-content no-padding">
+                            no-padding.
+                        </div>
+                    </div>
                 </b-col>
             </b-row>
         </b-container>
---
+++ src/admin/layout/WidgetBoxExample.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-row>
+        <b-col>
+            <div class="widget-box">
+                <div class="widget-title">
+                    <span class="icon"><i class="fa fa-arrows"></i></span>
+                    <h5>This a Titles</h5>
+                </div>
+                <div class="widget-content no-padding">
+                    widget-content no-padding.
+                </div>
+            </div>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        name: "WidgetBoxExample"
+    }
+</script>
+
+<style scoped>
+
+</style>
